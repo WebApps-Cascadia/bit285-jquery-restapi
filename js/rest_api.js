47e94cb926627cd5bd4e4d5d18f263a8a81cd09f
Debra Ward <debjward@yahoo.com>
2017-03-09 14:11:19 -0800

    commit

--- js/rest_api.js
+++ js/rest_api.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-  //Replace with your own API if you have it functioning for Toys, Colors, and Users
-  var baseURI = "http://toyauctionapi.azurewebsites.net/api/";
+    //Replace with your own API if you have it functioning for Toys, Colors, and Users
+
+    var baseURI = "http://localhost:62798/";//this is the address for the REST API I created on 3/7/17
+    //var baseURI = "http://toyauctionapi.azurewebsites.net/api/";
 	// Create jQuery objects representing the UI elements
     var usernametext = $('input#user_name');
     var toynametext = $('input#toy_name');
     var toylegstext = $('input#legs');
-		var colordropdown = $('select#toy_color');
-		var userdropdown = $('select#toy_owner');
+	var colordropdown = $('select#toy_color');
+	var userdropdown = $('select#toy_owner');
     var toydropdown = $('select#toy');
     // Populate the dropdown items using REST API
     getColors(colordropdown);
@@ -21,63 +23,100 @@ $(document).ready(function(){
           data: {
               // TODO: start with static date to see it working,
               // then use the jQuery .val() method to get the value of the username UI field
-
+              Name: usernametext.val()
           }
         }).done(function(result) {
             // display a message that the user is added
             $('<p></p>').html(result.Name + ' user added!').prependTo('section#user');
             // TODO: refresh the user dropdown list
-
+            getUsers(userdropdown);
         });
       });
 
 		// Add a Create Toy button click event handler using AJAX to create a new Toy
 			$('#addToy').click(function(){
-				$.ajax({
-					url: baseURI + "toys/",
-					method: "POST",
-					data: {
-              // TODO: start with static date to see it working,
-              // then use the jQuery .val() method to get the value of each UI field
-
-
-					}
+			    $.ajax({
+			        url: baseURI + "toys/",
+			        method: "POST",
+			        data: {
+			            // TODO: start with static date to see it working,
+			            // then use the jQuery .val() method to get the value of each UI field
+			            Name: toynametext.val(),
+			            NumberLegs: toylegstext.val(),
+			            ColorID: colordropdown.val(),
+			            OwnerID: userdropdown.val()
+			        }
         }).done(function(result) {
             // display a message that the toy is added
 						$('<p></p>').html(result.Name + ' toy added!').prependTo('section#toy');
             // TODO: refresh the toy dropdown list
-
-
+						getToys(toydropdown);
 				});
 			});
 
     // TODO: Add a  button click event handler using AJAX  to delete a Toy using HTTP DELETE
     			$('#deleteToy').click(function(){
 
-    				});
+    			    $.ajax({
+    			        url: baseURI + "toys/" + toydropdown.val(),
+    			        type: "DELETE",
+    			        dataType: "json",
+    			        data: {},
+
+    			        error: function (xhr, textStatus, errorThrown) {
+    			            alert("An error occurred!!");
+    			        }
+
+    			    }).done(function (result) {
+    			        // display a message that the toy is deleted
+    			        $('<p></p>').html(result.Name + ' toy deleted!').prependTo('section#toy');
+    			        // TODO: refresh the toy dropdown list
+    			        getToys(toysdropdown);
+
+    			    });
+
+    			});
 
 /*
  *  Helper methods for getting REST API data for esch dropdown
  */
 
-  function getColors(dropdown){ //Add colors to the dropdown
-		$.ajax({
-      url: baseURI + "colors/",
-      method: "GET"
-    }).done(function(result) {
-					console.log(result);  // Take a peek at the data
-          $.each(result, function(k, v) {
-              dropdown.append('<option value="' + v.ColorID + '">' + v.ColorName + '</option>');
-          });
-		});
-  };
-	function getUsers(dropdown){ // Add users to the dropdown
-    //TODO: write the AJAX method
+    			function getColors(dropdown) { //Add colors to the dropdown
+    			    $.ajax({
+    			        url: baseURI + "colors/",
+    			        method: "GET"
+    			    }).done(function (result) {
+    			        console.log(result);  // Take a peek at the data
+    			        $.each(result, function (k, v) {
+    			            dropdown.append('<option value="' + v.ColorID + '">' + v.ColorName + '</option>');
+    			        });
+    			    });
+    			};
+    			function getUsers(dropdown) { // Add users to the dropdown
+    			    //COMPLETE: TODO: write the AJAX method
+    			    $.ajax({
+    			        url: baseURI + "users/",
+    			        method: "GET"
+    			    }).done(function (result) {
+    			        console.log(result);  // Take a peek at the data
+    			        $.each(result, function (k, v) {
+    			            dropdown.append('<option value="' + v.UserID + '">' + v.Name + '</option>');
+    			        });
+    			    });
+    			};
+    // Add toys to the dropdown
+    			function getToys(dropdown) {
+    			    //COMPLETE: TODO: write the AJAX method
 
-   };
-  // Add toys to the dropdown
-    function getToys(dropdown){
-      //TODO: write the AJAX method
-    };
+    			    $.ajax({
+    			        url: baseURI + "toys/",
+    			        method: "GET"
+    			    }).done(function (result) {
+    			        console.log(result);  // Take a peek at the data
+    			        $.each(result, function (k, v) {
+    			            dropdown.append('<option value="' + v.ToyID + '">' + v.Name + '</option>');
+    			        });
+    			    });
+    			};
 
 });
